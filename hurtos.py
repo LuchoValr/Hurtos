a0c84af2858e2af72c3a278641443ec08f74a049
Luis Valera <luisvalera@usantotomas.edu.co>
2022-06-01 18:28:42 -0500

    Lectura_datos

--- hurtos.py
+++ hurtos.py
@@ -31,21 +31,55 @@ date
 #Visualizacion
 sexv=hur.groupby(by=['FECHA HECHO','GENERO']).sum().groupby(level=[0]).cumsum()
 sexv=sexv.reset_index()
-#Series de tiempo segun sexo
+#Grafico de Barras segun sexo
 #Enero
 sexv_enero=sexv.loc[sexv["FECHA HECHO"].between('2021-01-01', '2021-01-31')]
-sexv_enero=sexv_enero.pivot_table(index='FECHA HECHO',columns='GENERO',values='CANTIDAD')
-f2=sexv_enero[['FEMENINO','MASCULINO']].plot()
+fsexv1=sns.catplot(data=sexv_enero,x='GENERO',y='CANTIDAD',kind='bar',size=7)
 plt.show()
-#Series de tiempo segun edad
-#Enero
+#Febrero
+sexv_febrero=sexv.loc[sexv["FECHA HECHO"].between('2021-02-01', '2021-02-28')]
+fsexv2=sns.catplot(data=sexv_febrero,x='GENERO',y='CANTIDAD',kind='bar',size=7)
+plt.show()
+#Marzo
+sexv_marzo=sexv.loc[sexv["FECHA HECHO"].between('2021-03-01', '2021-03-31')]
+fsexv3=sns.catplot(data=sexv_marzo,x='GENERO',y='CANTIDAD',kind='bar',size=7)
+plt.show()
+#Grafico de barras segun edad
 agev=hur.groupby(by=['FECHA HECHO','EDAD PERSONA']).sum().groupby(level=[0]).cumsum()
 agev=agev.reset_index()
+#Enero
 agev_enero=agev.loc[agev["FECHA HECHO"].between('2021-01-01', '2021-01-31')]
-agev_enero=agev_enero.pivot_table(index='FECHA HECHO',columns='EDAD PERSONA',values='CANTIDAD')
-f3=agev_enero[['ADOLESCENTES','ADULTOS','MENORES']].plot()
+fagev=sns.catplot(data=agev_enero,x='EDAD PERSONA',y='CANTIDAD',kind='bar',size=7)
 plt.show()
-#Grafico de barras
+#Graficos de barras segun armas
+wepv=hur.groupby(by=['FECHA HECHO','ARMAS MEDIO']).sum().groupby(level=[0]).cumsum()
+wepv=wepv.reset_index()
+#Enero
+wepv_enero=wepv.loc[wepv["FECHA HECHO"].between('2021-01-01', '2021-01-31')]
+fwepv=sns.catplot(data=wepv_enero,x='ARMAS MEDIO',y='CANTIDAD',kind='bar',size=10)
+plt.show()
+#Graficas relacionando el genero con el arma usada
+sex_wep1=hur.filter(['ARMAS MEDIO','GENERO','FECHA HECHO','CANTIDAD'])
+#Masculino
+sex_wepm=sex_wep1[sex_wep1.GENERO.isin(['MASCULINO'])]
+sex_wepm=sex_wepm.groupby(by=['FECHA HECHO','ARMAS MEDIO','GENERO']).sum().groupby(level=[0]).cumsum()
+sex_wepm=sex_wepm.reset_index()
+#Enero
+sex_wep_enerom=sex_wepm.loc[sex_wepm["FECHA HECHO"].between('2021-01-01', '2021-01-31')]
+fsex_wepm_enero=sns.catplot(data=sex_wep_enerom,x='ARMAS MEDIO',y='CANTIDAD',kind='bar',size=10)
+plt.show()
+#Femenino
+sex_wepf=sex_wep1[sex_wep1.GENERO.isin(['FEMENINO'])]
+sex_wepf=sex_wepf.groupby(by=['FECHA HECHO','ARMAS MEDIO','GENERO']).sum().groupby(level=[0]).cumsum()
+sex_wepf=sex_wepf.reset_index()
+#Enero
+sex_wep_enerof=sex_wepf.loc[sex_wepf["FECHA HECHO"].between('2021-01-01', '2021-01-31')]
+fsex_wepf_enero=sns.catplot(data=sex_wep_enerof,x='ARMAS MEDIO',y='CANTIDAD',kind='bar',size=10)
+plt.show()
+
+
+
+#Grafico de barras totales genero
 f=sns.catplot(data=sex,x='GENERO',y='CANTIDAD',kind='bar',size=7)
 plt.show()
 #Heatmap
